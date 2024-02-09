b19286f078f14ddc3ffc54d0b9f6292abace0fd3
sree1411 <sreekanthsree141122@gmail.com>
2024-02-09 10:18:02 +0530

    uesEffect is applied to the Test1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
- 
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [books, setBooks] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/posts")
+      .then((res) => {
+        setBooks(res.data);
+      })
+      .catch();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-             <h1> hello people</h1>
-      </header>
+      <h1> hello people</h1>
+      <ul id="box">
+        {books.length >= 0 &&
+          books.map((book) => (
+            <li>
+              <h4> id:{book.id}</h4>
+              <h4> title:{book.title}</h4>
+              <h4> author: {book.author}</h4>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
